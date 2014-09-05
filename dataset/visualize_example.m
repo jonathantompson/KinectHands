7d9dd7d4f4bacc71d5e5797f6b38a5c0ac693b2e
Jonathan Tompson <jonathantompson@gmail.com>
2014-09-05 15:58:54 -0400

    Added the website files.

--- dataset/visualize_example.m
+++ dataset/visualize_example.m
@@ -1,60 +1,67 @@
 % Jonathan Tompson, New York University - 8/28/2014
 % This is a simple script to visualize a training example
 clearvars; close all; clc; rng(0);
+addpath('.\export_fig\');
 
-dataset_dir = 'F:\hand_data\dataset\train\';
-image_index = 70000;
-kinect_index = 3;
+dataset_dir = '.\train\';
+image_index = 1;
+kinect_index = 1;
 filename_prefix = sprintf('%d_%07d', kinect_index, image_index);
 
 %% Load and display an RGB example
 rgb = imread([dataset_dir, 'rgb_', filename_prefix, '.png']);
 figure;
+set(gcf, 'Position', [200 200 1800 400]);
+subplot_tight(1,6,1);
 imshow(rgb);
-title('RGB');
 
 %% Load and display a synthetic depth example
 % The top 8 bits of depth are packed into green and the lower 8 bits into blue.
 synthdepth = imread([dataset_dir, 'synthdepth_', filename_prefix, '.png']);
 synthdepth = uint16(synthdepth(:,:,3)) + bitsll(uint16(synthdepth(:,:,2)), 8);
-figure;
+subplot_tight(1,6,2);
 ind = find(synthdepth > 0);
 imshow(synthdepth, [min(synthdepth(ind))-10, max(synthdepth(ind))+10]);
-title('synthetic depth');
 
 %% Load and display a depth example
 % The top 8 bits of depth are packed into green and the lower 8 bits into blue.
 depth = imread([dataset_dir, 'depth_', filename_prefix, '.png']);
 depth = uint16(depth(:,:,3)) + bitsll(uint16(depth(:,:,2)), 8);
-figure;
+subplot_tight(1,6,3);
 imshow(depth, [0, max(depth(:))]);
-title('depth');
 
 %% Load the UVD Coefficients and display them on the depth image
 load([dataset_dir, 'joint_data.mat']);
 jnt_uvd = squeeze(joint_uvd(kinect_index, image_index, :, :));
 jnt_colors = rand(size(jnt_uvd,1), 3);
 hold on;
-scatter(jnt_uvd(:,1), jnt_uvd(:,2), 50, jnt_colors, 'filled');
+scatter(jnt_uvd(:,1), jnt_uvd(:,2), 20, jnt_colors, 'filled');
 
 %% Visualize the depth in 3D
 uvd = convert_depth_to_uvd(depth);
 xyz = convert_uvd_to_xyz(uvd);
 % Decimate the image (otherwise rendering will be too slow)
-decimation = 5;
+decimation = 4;
 xyz_decimated = xyz(1:decimation:end, 1:decimation:end, :);
 points = reshape(xyz_decimated, size(xyz_decimated,1)*size(xyz_decimated,2), 3);
+% Collect the points within the AABBOX of the non-background points
+body_points = points(find(points(:,3) < 2000),:);
+axis_bounds = [min(body_points(:,1)) max(body_points(:,1)) ...
+  min(body_points(:,3)) max(body_points(:,3)) ...
+  min(body_points(:,2)) max(body_points(:,2))];
 % Visualize the entire point cloud
-figure;
-set(gcf, 'Position', [200 200 800 600]);
-plot3(points(:,1), points(:,3), points(:,2), '.', 'MarkerSize', 0.75);
+subplot_tight(1,6,4,[0.05,0.05]);
+plot3(body_points(:,1), body_points(:,3), body_points(:,2), '.', 'MarkerSize', 1.5);
+axis(axis_bounds);
+view(45,20);
 set(gcf,'renderer','opengl'); axis vis3d; axis equal; hold on; grid on;
-title('Kinect point cloud');
+set(gca,'xticklabel',[]);
+set(gca,'yticklabel',[]);
+set(gca,'zticklabel',[]);
 
 %% Visualize the hand and the joints in 3D
-figure;
 for i = 1:2
-  subplot(1,2,i);
+  subplot_tight(1,6,7-i,[0.05,0.05]);
   if i == 1
     uvd = convert_depth_to_uvd(depth);
   else
@@ -75,14 +82,14 @@ for i = 1:2
     points(:,3) >= axis_bounds(3) & points(:,3) <= axis_bounds(4));
   points = points(ipnts, :);
   colors = double(colors(ipnts, :))/255;
-  set(gcf, 'Position', [200 200 1200 600]);
-  plot3(points(:,1), points(:,3), points(:,2), '.'); 
+  plot3(points(:,1), points(:,3), points(:,2), '.', 'MarkerSize', 1.5); 
   set(gcf,'renderer','opengl'); axis vis3d; axis equal; hold on; grid on;
   scatter3(hand_points(:,1), hand_points(:,3), hand_points(:,2), 50, jnt_colors, 'Fill','LineWidth', 0.5);
   axis(axis_bounds);
-  if i == 1
-    title('Kinect point cloud (just hand)');
-  else
-    title('Synthetic point cloud (just hand)');
-  end
+  set(gca,'xticklabel',[]);
+  set(gca,'yticklabel',[]);
+  set(gca,'zticklabel',[]);
 end
+
+set(gcf, 'Color', [1 1 1]);
+export_fig('sample.png', gcf, '-png', '-a4');
